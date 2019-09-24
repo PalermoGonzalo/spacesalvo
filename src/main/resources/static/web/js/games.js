d0a2e59cdda507b743daa2ffb4341a0259a33276
Gonzalo Lucas Palermo <palermogonzalo@gmail.com>
2019-09-24 14:23:07 -0300

    Se agregan dashboard de puntuaciones

--- src/main/resources/static/web/js/games.js
+++ src/main/resources/static/web/js/games.js
@@ -18,11 +18,45 @@ var app = new Vue({
                          this.app.scores = myJson;
                          console.log(this.app.scores);
                          myJson.forEach(function(score){
-                            this.app.players.push(score.player);
+                            let newPlayer = 1;
+                            this.app.players.forEach(function(player){
+                                if(player.player == score.player){
+                                    console.log("old Player");
+                                    if(score.score == 1){
+                                        player['win'] += 1;
+                                        player['totalScore'] += 1;
+                                    }else if(score.score != 0){
+                                        player['ties'] += 1;
+                                        player['totalScore'] += 0.5;
+                                    }else{
+                                        player['lost'] += 1;
+                                    }
+                                    newPlayer = 0;
+                                }
+                            });
+                            if(newPlayer == 1){
+                                console.log("new player");
+                                let playerStats = [];
+                                playerStats['player'] = score.player;
+                                playerStats['win'] = 0;
+                                playerStats['ties'] = 0;
+                                playerStats['lost'] = 0;
+                                playerStats['totalScore'] = 0;
+                                if(score.score == 1){
+                                    playerStats['win'] = 1;
+                                    playerStats['totalScore'] = 1;
+                                }else if(score.score != 0){
+                                    playerStats['ties'] = 1;
+                                    playerStats['totalScore'] = 0.5;
+                                }else{
+                                    playerStats['lost'] = 1;
+                                }
+                                this.app.players.push(playerStats);
+                            }
                          });
-                         return myJson;
+                     return myJson;
                      });
-             }
+                 }
      },
      computed:{
      }
